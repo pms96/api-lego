c985549f320c667b814830b35e12e76271e9db50
pablo <ppaabloms96@gmail.com>
2025-03-14 23:01:28 +0100

    user stasts

--- routes/api.php
+++ routes/api.php
@@ -16,7 +16,7 @@ Route::get('/brickheadz/{id}', [BrickheadzController::class, 'show']);
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/user/collection', [CollectionController::class, 'index']);
+    Route::get('/user/{user}/dashboard/stats', [DashboardController::class, 'getStats']);
     Route::post('/user/collection', [CollectionController::class, 'store']);
     Route::put('/user/collection/{collection}', [CollectionController::class, 'update']);
-    Route::get('/user/{user}/dashboard/stats', [DashboardController::class, 'getStats']);
 });
